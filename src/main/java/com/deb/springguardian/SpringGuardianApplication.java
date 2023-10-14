c6a25cf699322301d4fe776b8d80025abe2ddfd4
DebAnand200 <beheradebananda000@gmail.com>
2023-10-15 01:03:34 +0530

    Initial commit

---
+++ src/main/java/com/deb/springguardian/SpringGuardianApplication.java
@@ -0,0 +1,18 @@
+package com.deb.springguardian;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGuardianApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGuardianApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Hashnode");
+	}
+}
---
+++ src/test/java/com/deb/springguardian/SpringGuardianApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deb.springguardian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGuardianApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
